8b0e4bdd25e6a577f81705d6cffd23cb709246f7
alsv06003 <alsv06003@E-PW0234JW>
2023-09-20 10:01:00 +0200

    Add project files.

---
+++ inlämmning 2.4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Uppgift_2._4
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("skriv in lönen för första arbetaren");
+            double lön1 = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("skriv in lönen för andra arbetaren");
+            double lön2=double.Parse(Console.ReadLine());
+
+            Console.WriteLine("skriv in lönen för tredje arbetaren");
+            double lön3=double.Parse(Console.ReadLine());
+
+            double summa = (lön1 + lön2 + lön3)/3;
+
+            Console.WriteLine("medellönen av allas löner är " + summa);
+        }
+    }
+}
